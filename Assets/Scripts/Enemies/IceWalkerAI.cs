a265227bdfe62683c6b8355cdec14ec961c6343d
Kadameck <adameckkevin@gmail.com>
2020-11-04 18:21:31 +0100

    Test Comit
    Just a Test Comit where i change a Debug.Log() output

--- Assets/Scripts/Enemies/IceWalkerAI.cs
+++ Assets/Scripts/Enemies/IceWalkerAI.cs
@@ -13,6 +13,6 @@ public class IceWalkerAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Das ist nur ein Script zum Testen");
+        Debug.Log("Das ist nur ein Script zum Testen und kann daher ignoriert werden");
     }
 }
