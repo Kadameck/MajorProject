293dbac96d15282accfaaa5c7590cab87f6f6ec4
Kadameck <adameckkevin@gmail.com>
2020-12-04 07:35:14 +0100

    Environment and Grove Fade in
    - Level Design
    - The Grove is only visible if the player enters a trigger now

---
+++ Assets/Scripts/EnvironmentScripts/GroveFadeIn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroveFadeIn : MonoBehaviour
+{
+    [SerializeField]
+    AnimationClip clip;
+
+    [SerializeField]
+    Animator anim;
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            anim.SetBool("isActive", true);
+            StartCoroutine(DestroyAfterAnim());
+        }
+    }
+
+    IEnumerator DestroyAfterAnim()
+    {
+        yield return new WaitForSeconds(clip.length);
+
+        Destroy(this.gameObject);
+    }
+}
