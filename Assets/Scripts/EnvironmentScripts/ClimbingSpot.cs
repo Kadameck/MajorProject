b74bc2528726a9f62f27f4d62926971fc0480294
Kadameck <adameckkevin@gmail.com>
2020-12-07 04:16:56 +0100

    Climbing Funktion

---
+++ Assets/Scripts/EnvironmentScripts/ClimbingSpot.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClimbingSpot : MonoBehaviour
+{
+    [SerializeField]
+    char upKey;
+    [SerializeField]
+    char downKey;
+
+    private GameObject child;
+    private bool memoryPlayer = false;
+
+    // Start is called before the first frame update
+
+    private void Update()
+    {
+        if (child != null)
+        {
+            if (upKey == 'w' && Input.GetKey(KeyCode.W) ||
+               upKey == 'd' && Input.GetKey(KeyCode.D) ||
+               upKey == 'a' && Input.GetKey(KeyCode.A))
+            {
+                child.transform.Translate(Vector3.up * 0.1f);
+            }
+            else if (downKey == 'w' && Input.GetKey(KeyCode.S) ||
+                    downKey == 'd' && Input.GetKey(KeyCode.D) ||
+                    downKey == 'a' && Input.GetKey(KeyCode.A))
+            {
+                child.transform.Translate(Vector3.down * 0.1f);
+            }
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player") && memoryPlayer == false && other.transform.position.y < transform.position.y)
+        {
+            other.GetComponent<ShamanControl>().isClimbing = true;
+            other.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            other.GetComponent<Rigidbody>().useGravity = false;
+            other.transform.SetParent(this.transform);
+            child = other.gameObject;
+            
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            other.GetComponent<ShamanControl>().isClimbing = false;
+            other.transform.SetParent(null);
+            other.GetComponent<Rigidbody>().useGravity = true;
+            other.GetComponent<Rigidbody>().AddForce((Vector3.up + other.transform.forward) * 1000, ForceMode.Impulse);
+            child = null;
+            memoryPlayer = true;
+            StartCoroutine(test());
+        }
+    }
+
+
+    IEnumerator test()
+    {
+        yield return new WaitForSeconds(0.5f);
+        memoryPlayer = false;
+        
+    }
+}
--- Assets/Scripts/PlayerScripts/ShamanControl.cs
+++ Assets/Scripts/PlayerScripts/ShamanControl.cs
@@ -15,6 +15,9 @@ public class ShamanControl : MonoBehaviour
     [SerializeField]
     float rotationSpeed = 10;
 
+    [HideInInspector]
+    public bool isClimbing = false;
+
     // Movement y value
     private float yDirect;
 
@@ -40,38 +43,41 @@ public class ShamanControl : MonoBehaviour
     /// </summary>
     private void Movement()
     {
-        // Checks if the player wants to move
-        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        if (!isClimbing)
         {
-            // Takes the movement direktion 
-            float xMove = Input.GetAxisRaw("Horizontal");
-            float zMove = Input.GetAxisRaw("Vertical");
-
-            // Takes the new movement Direction
-            Vector3 targetDir = new Vector3(xMove, 0.0f, zMove).normalized;
-
-            // Calls the PlayerRotation function
-            PlayerRotation(targetDir);
-
-            // Calls the GroundScann function to check whether there is ground under the player and, if so, in which y direction the player must move
-            if (GroundScan())
+            // Checks if the player wants to move
+            if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
             {
-                // Checks if the player should sneak
-                if (Sneak())
+                // Takes the movement direktion 
+                float xMove = Input.GetAxisRaw("Horizontal");
+                float zMove = Input.GetAxisRaw("Vertical");
+
+                // Takes the new movement Direction
+                Vector3 targetDir = new Vector3(xMove, 0.0f, zMove).normalized;
+
+                // Calls the PlayerRotation function
+                PlayerRotation(targetDir);
+
+                // Calls the GroundScann function to check whether there is ground under the player and, if so, in which y direction the player must move
+                if (GroundScan())
                 {
-                    // Moves the player in sneak speed
-                    rb.velocity = new Vector3(xMove, yDirect, zMove) * sneakSpeed * Time.deltaTime;
+                    // Checks if the player should sneak
+                    if (Sneak())
+                    {
+                        // Moves the player in sneak speed
+                        rb.velocity = new Vector3(xMove, yDirect, zMove) * sneakSpeed * Time.deltaTime;
+                    }
+                    else
+                    {
+                        // Moves the player normal speed
+                        rb.velocity = new Vector3(xMove, yDirect, zMove) * walkSpeed * Time.deltaTime;
+                    }
                 }
                 else
                 {
-                    // Moves the player normal speed
-                    rb.velocity = new Vector3(xMove, yDirect, zMove) * walkSpeed * Time.deltaTime;
+                    // Drops the player
+                    rb.velocity += new Vector3(0.0f, -1.0f, 0.0f);
                 }
-            }
-            else
-            {
-                // Drops the player
-                rb.velocity += new Vector3(0.0f, -1.0f, 0.0f);
             }
         }
     }
