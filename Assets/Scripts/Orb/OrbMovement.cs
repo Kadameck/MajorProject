c98aea64c2e11c55e94be4f9c34abbd823fa1ea5
Kadameck <adameckkevin@gmail.com>
2020-12-10 01:24:57 +0100

    Orb
    - Implement an Orb taht is following Waypoints to lead the player

---
+++ Assets/Scripts/Orb/OrbMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbMovement : MonoBehaviour
+{
+    [SerializeField]
+    float orbSpeed = 0.1f;
+
+    [SerializeField]
+    List<Transform> wayPoints;
+
+    private Transform orbTrans;
+    private Transform playerTrans;
+    private int iterator = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        orbTrans = GameObject.FindGameObjectWithTag("Orb").transform;
+        playerTrans = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newDirection = Vector3.zero;
+
+        if (Vector3.Distance(orbTrans.position, playerTrans.position) < 20)
+        {
+            orbTrans.LookAt(wayPoints[iterator].position);
+            orbTrans.Translate(new Vector3(0.0f, 0.0f, Time.deltaTime * orbSpeed));
+
+            if (Vector3.Distance(orbTrans.position, wayPoints[iterator].position) < 2)
+            {
+                if(iterator == wayPoints.Count-1)
+                {
+                    Destroy(orbTrans.gameObject);
+                    Destroy(this);
+                }
+                else
+                {
+                    iterator++;
+                }
+            }
+        }
+    }
+}
