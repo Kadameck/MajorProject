51e2ba16f269a08b4b8f33994863b1a709c8df01
Kadameck <adameckkevin@gmail.com>
2020-12-18 00:40:07 +0100

    Player gravity fix
    The player will no longer sometimes float above the ground after going down a slope

--- Assets/Scripts/PlayerScripts/ShamanControl.cs
+++ Assets/Scripts/PlayerScripts/ShamanControl.cs
@@ -46,9 +46,8 @@ public class ShamanControl : MonoBehaviour
     // FixedUpdate is called regularly at a fixed interval
     void FixedUpdate()
     {
-        //UseMagic();
-        Interact();
         Movement();
+        Interact();
     }
 
 
@@ -60,22 +59,23 @@ public class ShamanControl : MonoBehaviour
         // Checks if the normal Movement should be executed or if the player is climbing
         if (!isClimbing)
         {
-            // Checks if the player wants to move
-            if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+            if(GroundScan())
             {
-                // Takes the movement direktion 
-                float xMove = Input.GetAxisRaw("Horizontal");
-                float zMove = Input.GetAxisRaw("Vertical");
-
-                // Takes the new movement Direction
-                Vector3 targetDir = new Vector3(xMove, 0.0f, zMove).normalized;
-
-                // Calls the PlayerRotation function
-                PlayerRotation(targetDir);
-
-                // Calls the GroundScann function to check whether there is ground under the player and, if so, in which y direction the player must move
-                if (GroundScan())
+                if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
                 {
+                    // Takes the movement direktion 
+                    float xMove = Input.GetAxisRaw("Horizontal");
+                    float zMove = Input.GetAxisRaw("Vertical");
+
+                    // Takes the new movement Direction
+                    Vector3 targetDir = new Vector3(xMove, 0.0f, zMove).normalized;
+
+                    // Calls the PlayerRotation function
+                    PlayerRotation(targetDir);
+
+                    // Calls the GroundScann function to check whether there is ground under the player and, if so, in which y direction the player must move
+                    //    if (GroundScan())
+                    //  {
                     // Checks if the player should sneak
                     if (Sneak())
                     {
@@ -87,13 +87,20 @@ public class ShamanControl : MonoBehaviour
                         // Moves the player normal speed
                         rb.velocity = new Vector3(xMove, yDirect, zMove) * walkSpeed * Time.deltaTime;
                     }
-                }
-                else
-                {
+                    // }
+                    // else
+                    //{
                     // Drops the player
                     rb.velocity += new Vector3(0.0f, -1.0f, 0.0f);
+                    //}
                 }
             }
+            else
+            {
+                rb.velocity += new Vector3(0.0f, -1.0f, 0.0f);
+            }
+            // Checks if the player wants to move
+       
         }
     }
 
