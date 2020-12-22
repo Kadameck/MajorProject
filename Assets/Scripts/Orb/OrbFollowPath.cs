728d6e61821953a7bb7d2da114272feb45690965
Kadameck <adameckkevin@gmail.com>
2020-12-22 11:38:56 +0100

    Orb path visualization (TestDrawWay.cs)
    A script that visualizes a sequence of Bézier curves. This visualization corresponds to the path which (if the same waypoints are given) is finally calculated in "OrbMovement.cs"

---
+++ Assets/Scripts/Orb/OrbFollowPath.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbFollowPath : MonoBehaviour
+{
+    // Die Objekte die die Waypoints enthalten
+    [SerializeField]
+    GameObject[] pathContainer;
+
+    private List<Vector3> positions = new List<Vector3>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Loopt durch die Childobjekte des ersten pathcontainers
+        for (int i = 1; i < pathContainer[0].transform.childCount; i++)
+        {
+            // fügt die childobjekte der liste zu damit man einen start zum berechnen hat
+            // Wird dann später das letzte child ereicht muss diese liste gecleard und mit den positionen der childern des nächsten pathContainer befüllt werden
+            positions.Add(pathContainer[0].transform.GetChild(i).position);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Orb/TestDrawWay.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDrawWay : MonoBehaviour
+{
+    public GameObject wpHolder;
+
+
+   // private GameObject[] waypoints;
+
+    int points = 50;
+    int i = 0;
+
+    Vector3 gizmoPositions;
+
+    Vector3 p0;
+    Vector3 p1;
+    Vector3 p2;
+
+    private void OnDrawGizmos()
+    {
+        for (int x = 0; x < wpHolder.transform.childCount; x += 2)
+        {
+            p0 = wpHolder.transform.GetChild(x).transform.position;
+            p1 = wpHolder.transform.GetChild(x+1).transform.position;
+            p2 = wpHolder.transform.GetChild(x+2).transform.position;
+
+            for (float i = 0; i < points + 1; i++)
+            {
+                float t = i / (float)points;
+                float u = 1 - t;
+                float uSquare = u * u;
+                float tSquare = t * t;
+
+                Vector3 currentPosition = uSquare * p0;
+                currentPosition += 2 * u * t * p1;
+                currentPosition += tSquare * p2;
+
+                gizmoPositions = currentPosition;
+                Gizmos.color = Color.red;
+
+                Gizmos.DrawSphere(gizmoPositions, 0.25f);
+            }
+
+            Gizmos.color = Color.white;
+            Gizmos.DrawLine(p0, p1);
+            Gizmos.DrawLine(p1, p2);
+        }
+    }
+}
