32f3c54926748252546cd925188351558e4acd44
Kadameck <adameckkevin@gmail.com>
2020-12-29 22:25:08 +0100

    Better player orientation
    Creation of HideTreesInRange.cs:
     Objects that have a collider and the tag "ObstacleTree"
     now disappear if they are inside the "TriggerHolder" trigger and their world position has a lower Z-value than the player
    
    In addition, the LOD properties were removed from all trees in the Obstacle Prefabs (these are not necessary anyway) and the trees correspondingly provided with colliders that are only used for the HideTreesInRange.cs. In addition, the "ObstacleTree" tag was assigned to each of these trees.

---
+++ Assets/Scripts/PlayerScripts/HideTreesInRange.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideTreesInRange : MonoBehaviour
+{
+    private List<GameObject> hiddenTrees = new List<GameObject>();
+
+    private void OnTriggerStay(Collider other)
+    {
+        // Ist das im trigger ein ObstacleTree und ist dieser "unterhalb" des spielers und ist dieser noch nicht unsichtbar
+        if(other.gameObject.CompareTag("ObstacleTree") && 
+            other.gameObject.transform.position.z < transform.position.z &&
+            !hiddenTrees.Contains(other.gameObject))
+        {
+            hiddenTrees.Add(other.gameObject);
+            other.gameObject.GetComponent<Renderer>().enabled = false;
+        }
+        // ist es ein obstacleBaum und dieser hat einen größeren z wert als der spieler und er ist aber auf der unsichtbar liste
+        else if(other.gameObject.CompareTag("ObstacleTree") && 
+                 other.gameObject.transform.position.z > transform.position.z && 
+                 hiddenTrees.Contains(other.gameObject))
+        {
+            other.gameObject.GetComponent<Renderer>().enabled = true;
+            hiddenTrees.Remove(other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("ObstacleTree"))
+        {
+            hiddenTrees.Remove(other.gameObject);
+            other.gameObject.GetComponent<Renderer>().enabled = true;
+        }
+    }
+}
