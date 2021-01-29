978524d4f6c61c130ccc684c9c637ccb5f847b39
Kadameck <adameckkevin@gmail.com>
2021-01-29 16:23:55 +0100

    Create PauseParticleEffect Script to try to visualize the time stone

---
+++ Assets/Scripts/EnvironmentScripts/PauseParticleSystem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseParticleSystem : MonoBehaviour
+{
+    [SerializeField]
+    ParticleSystem system;
+    [SerializeField]
+    float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(PauseTimer());
+        system.Pause();
+    }
+
+    private IEnumerator PauseTimer()
+    {
+        float delayTimer = 0;
+
+        while(delayTimer <= timer)
+        {
+            delayTimer += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        system.Pause();
+        yield return new WaitForSeconds(1);
+        Destroy(this.gameObject.GetComponent<PauseParticleSystem>());
+    }
+}
